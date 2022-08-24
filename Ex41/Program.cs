ecffe032f107bae5df653000edf270fc8f3bfbeb
master <frami@framin1991.com>
2022-08-24 21:26:45 +0500

    initial commit

---
+++ Ex41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] ReadInputArray ()
+{
+    Console.WriteLine("Введите желаемое количество чисел: ");
+    int count = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void Above0 (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    Console.WriteLine($"Введено {count} чисел больше нуля.");
+}
+
+int[] array = ReadInputArray();
+Console.Write("Введенные числа: ");
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine();
+Above0(array);
---
+++ Ex43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] ReadInputArray ()
+{
+    int count = 0;
+    double[] array = new double[4];
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine($"Введите значение b{i + 1}: ");
+        array[count] = Convert.ToDouble(Console.ReadLine());
+        count++;
+        Console.WriteLine($"Введите значение k{i + 1}: ");
+        array[count] = Convert.ToDouble(Console.ReadLine());
+        count++;
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void CrossPoint (double[] arr)
+{
+    double b1 = arr[0];
+    double k1 = arr[1];
+    double b2 = arr[2];
+    double k2 = arr[3];
+    if (k1!= k2)
+    {
+        double y = (k2 * b1 - b2 * k1) / (k2 - k1);
+        double x = (y - b1) / k1;
+        Console.WriteLine("Координаты точки пересечения:");
+        Console.WriteLine($"y = {Math.Round(y, 1)}");
+        Console.WriteLine($"x = {Math.Round(x, 1)}");
+    }
+    else Console.WriteLine("Линии не пересекаются.");
+}
+
+double[] array = ReadInputArray();
+CrossPoint(array);
